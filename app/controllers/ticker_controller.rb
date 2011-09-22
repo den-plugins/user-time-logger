77f3c1fc9f9279317b108978f41f0511d590be44
Nico Bantatua <nbantatua@gmail.com>
2011-09-22 18:39:48 +0800

    merged the By Project Type filter

--- app/controllers/ticker_controller.rb
+++ app/controllers/ticker_controller.rb
@@ -187,7 +187,25 @@ class TickerController < ApplicationController
   
 
   def update_time_entries_simple
-    
+   	@billing_model = CustomField.find_by_name('Billing Model')
+
+  	if @billing_model
+	  	@billing_model_values = [["Please Select", "0"]]
+	  	@billing_model.possible_values.each do |v|
+	  		@billing_model_values << v
+	  	end
+	  end
+	  
+   	@project_type = CustomField.find_by_name('Project Type')
+
+  	if @project_type
+	  	@project_type_values = [["Please Select", "0"]]
+	  	@project_type.possible_values.each_line do |line|
+	  		@project_type_values << line
+	  	end
+	  end	  
+
+
     retrieve_date_range(params[:period_type],params[:period])
     @columns = (params[:columns] && %w(year month week day).include?(params[:columns])) ? params[:columns] : 'month'
     @query = (params[:query].blank?)? "user" : params[:query]
@@ -208,7 +226,20 @@ class TickerController < ApplicationController
     project_select = "id, name"
     project_order = "name asc"
     
-    
+    @billing = params[:billing_model]
+    @project_billing_ids = []
+    billings = CustomValue.find_all_by_value(@billing)
+    billings.each do |x|
+    	@project_billing_ids << x.customized_id
+    end if billings
+
+    @projtype = params[:project_type]
+    @project_type_ids = []
+    projtypes = CustomValue.find_all_by_value(@projtype)
+    projtypes.each do |x|
+    	@project_type_ids << x.customized_id
+    end if projtypes
+
     if @query == "user"
       available_user_conditions = []
       available_user_conditions << "\"users\".\"status\" = 1"
@@ -232,11 +263,17 @@ class TickerController < ApplicationController
                                         :order => project_order )
       @selected_projects = []
     else
+
+    	
+
       available_project_conditions = []
       available_project_conditions << ( (@selected_acctype == 0)? nil : "\"projects\".\"acctg_type\" = #{params[:acctype]}")
       available_project_conditions << ( (params[:selectedprojects].blank?)? nil : "id not in (#{params[:selectedprojects].join(',')})")
+			available_project_conditions << ("id in (#{@project_billing_ids.join(',')})") if !@project_billing_ids.empty? and @billing != "0"
+			available_project_conditions << ("id in (#{@project_type_ids.join(',')})") if !@project_type_ids.empty? and @projtype != "0"
       available_project_conditions = available_project_conditions.compact.join(" and ")
       #available_project_conditions = ( (params[:selectedprojects].blank?)? "" : "id not in (#{params[:selectedprojects].join(',')})")
+
       @available_projects = Project.active.all(:select => project_select,
                                         :conditions => available_project_conditions,
                                         :order => project_order)
